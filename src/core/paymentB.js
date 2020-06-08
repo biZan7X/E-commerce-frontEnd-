28ed330962d7f7f5d787e8c6fa691335859f40fb
biZan7X <bizantechX@gmail.com>
2020-06-08 14:31:13 +0530

    fixed payment UI

--- src/core/paymentB.js
+++ src/core/paymentB.js
@@ -1,36 +1,37 @@
+//PaymentB.js
 import React, { useState, useEffect } from "react";
 import { loadCart, cartEmpty } from "./helper/CartHelper";
 import { Link } from "react-router-dom";
-import { getmeToken, processPayment } from "./helper/paymentBHelper"; //request making funcs to process payment
+import { getmeToken, processPayment } from "./helper/paymentBHelper";
 import { createOrder } from "./helper/orderHelper";
 import { isAuthenticated } from "../auth/helper";
 import DropIn from "braintree-web-drop-in-react";
 
-const PaymentB = ({ products, setReload = (f) => f, reload = undefined }) => {
+const Paymentb = ({ products, setReload = (f) => f, reload = undefined }) => {
    const [info, setInfo] = useState({
       loading: false,
       success: false,
       clientToken: null,
       error: "",
-      instance: {}, //it is nothing it automatically gets filled up by the requests
+      instance: {},
    });
 
    const userId = isAuthenticated() && isAuthenticated().user._id;
-   const token = isAuthenticated() && isAuthenticated().user.token;
+   const token = isAuthenticated() && isAuthenticated().token;
 
    const getToken = (userId, token) => {
       getmeToken(userId, token).then((info) => {
-         console.log("INFORMATION: ", info);
-         if (info.error) {
+         console.log("INFORMATION", info);
+         if (info?.error) {
             setInfo({ ...info, error: info.error });
          } else {
-            const clientToken = info.clientToken;
+            const clientToken = info?.clientToken || "empty";
             setInfo({ clientToken });
          }
       });
    };
 
-   /*const showbtDropIn = () => {
+   const showbtdropIn = () => {
       return (
          <div>
             {info.clientToken !== null && products.length > 0 ? (
@@ -41,27 +42,59 @@ const PaymentB = ({ products, setReload = (f) => f, reload = undefined }) => {
                   />
                   <button
                      className="btn btn-block btn-success"
-                     onClick={() => {}}
+                     onClick={onPurchase}
                   >
                      Buy
                   </button>
                </div>
             ) : (
-               <h2>Add some items to proceed</h2>
+               <h3>Please add something to cart</h3>
             )}
          </div>
       );
-   };*/
+   };
 
    useEffect(() => {
       getToken(userId, token);
    }, []);
 
+   const onPurchase = () => {
+      setInfo({ loading: true });
+      let nonce;
+      let getNonce = info.instance.requestPaymentMethod().then((data) => {
+         nonce = data.nonce;
+         const paymentData = {
+            paymentMethodNonce: nonce,
+            amount: getAmount(), //convert to rupees
+         };
+         processPayment(userId, token, paymentData)
+            .then((Response) => {
+               setInfo({ ...info, success: Response.success, loading: false });
+               console.log("PAYMENT SUCCESS");
+            })
+            .catch((error) => {
+               setInfo({ loading: false, success: false });
+               console.log("PAYMENT FAILED");
+            });
+      });
+   };
+
+   const getAmount = () => {
+      let amount = 0;
+      products.map((p) => {
+         amount = amount + p.price;
+      });
+      return amount;
+   };
+
    return (
       <div>
-         <h2>Checkout & Payments</h2>
+         <h3>Payments</h3>
+         {showbtdropIn()}
+         <h3></h3>
+         <h3>Your bill is : ₹ {getAmount()}</h3>
       </div>
    );
 };
 
-export default PaymentB;
+export default Paymentb;
