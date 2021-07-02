d62c356241ec90ef9d696fe0d26a3da2227da3d3
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-07-02 22:54:55 +0530

    styles

--- src/core/Base.js
+++ src/core/Base.js
@@ -10,35 +10,35 @@ LOGO:
 */
 
 const Base = ({
-   title = "My Title", //default arguments
-   description = "My description",
-   className = "bg-dark text-white text-center py-4",
-   children, //once we use children , this base component acts as an enclosing parent for the chldren component
+	title = "My Title", //default arguments
+	description = "My description",
+	className = "bg-dark text-white text-center py-4",
+	children, //once we use children , this base component acts as an enclosing parent for the chldren component
 }) => {
-   return (
-      <div>
-         <Menu />
-         <div className="container-fluid">
-            <div className="jumbotron bg-dark text-white text-center">
-               <h2 className="display-4">{title}</h2>
-               <p className="lead">{description}</p>
-            </div>
-            <div className={className}>{children}</div>
-         </div>
-         <footer className="footer bg-dark mt-auto py-3">
-            <div className="container-fluid bg-success text-white text-center py-3">
-               <h4>If you got any questions feel free to reach us!</h4>
-               <button className="btn btn-warning btn-lg">Contact Us</button>
-            </div>
-            <div className="container">
-               <span className="text-muted">
-                  Amazing <span className="text-white">biZan Merch Store©</span>{" "}
-                  2020-21
-               </span>
-            </div>
-         </footer>
-      </div>
-   );
+	return (
+		<div>
+			<Menu />
+			<div className="container-fluid">
+				<div className="jumbotron bg-dark text-white text-center">
+					<h2 className="display-4">{title}</h2>
+					<p className="lead">{description}</p>
+				</div>
+				<div className={className}>{children}</div>
+			</div>
+			<footer className="footer bg-dark mt-3 mb-1 py-3">
+				<div className="container-fluid bg-success text-white text-center py-3">
+					<h4>If you got any questions feel free to reach us!</h4>
+					<button className="btn btn-warning btn-lg">Contact Us</button>
+				</div>
+				<div className="container">
+					<span className="text-muted">
+						Amazing <span className="text-white">biZan Merch Store©</span>{" "}
+						2020-21
+					</span>
+				</div>
+			</footer>
+		</div>
+	);
 };
 
 export default Base;
--- src/core/Home.js
+++ src/core/Home.js
@@ -6,43 +6,56 @@ import Card from "./Card";
 import getProducts from "./helper/coreapicalls";
 
 export default function Home() {
-   const [products, setProducts] = useState([]);
-   const [errors, setErrors] = useState(false);
+	const [products, setProducts] = useState([]);
+	const [errors, setErrors] = useState(false);
 
-   const loadAllProducts = () => {
-      getProducts().then((data) => {
-         if (data.error) {
-            setErrors(data.error);
-         } else {
-            setProducts(data);
-         }
-      });
-   };
+	const loadAllProducts = () => {
+		getProducts().then((data) => {
+			if (data.error) {
+				setErrors(data.error);
+			} else {
+				setProducts(data);
+			}
+		});
+	};
 
-   useEffect(() => {
-      loadAllProducts();
-   }, []);
+	useEffect(() => {
+		loadAllProducts();
+	}, []);
 
-   return (
-      <Base
-         title="Home Page"
-         description="The Official biZan Merch Store"
-         className="bg-dark text-white text-center"
-      >
-         <div className="row text-center">
-            <h1 className="text-white py-4">
-               <u>Merchandises</u>:
-            </h1>
-            <div className="row">
-               {products.map((product, index) => {
-                  return (
-                     <div key={index} className="col-4 mb-4">
-                        <Card product={product} />
-                     </div>
-                  );
-               })}
-            </div>
-         </div>
-      </Base>
-   );
+	return (
+		<Base
+			title="Home Page"
+			description="The Official biZan Merch Store"
+			className="bg-dark text-white text-center"
+		>
+			<div className="row text-center">
+				<h1 className="text-white py-4">
+					<u>Merchandises</u>:
+				</h1>
+				<div className="row">
+					{products.map((product, index) => {
+						return (
+							<div key={index} className="col-4 mb-4">
+								<Card product={product} />
+							</div>
+						);
+					})}
+				</div>
+			</div>
+			<a
+				href="https://dogechats.netlify.app/"
+				style={{
+					border: "blue solid 2px",
+					borderRadius: "2rem",
+					padding: "0.5rem 0.75rem",
+					color: "white",
+					cursor: "pointer",
+					margin: "2rem",
+				}}
+			>
+				Discussion Room
+			</a>
+		</Base>
+	);
 }
